590b14ef5bc4acdb241efedce1404deac14df146
bjakus <bjakus@gmail.com>
2015-11-21 13:42:30 +0100

    inital setup

---
+++ PaintballApp/PaintballApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaintballApp
+//
+//  Created by Bojan Jakuš on 21.11.2015..
+//  Copyright © 2015. Bastion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
