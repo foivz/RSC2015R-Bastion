96a3713212a62bd601dfc43e85e883bcc2238b06
Josip Trupina <jtrupina@gmail.com>
2015-11-21 15:30:40 +0100

    added api login, register

--- web/app/Http/routes.php
+++ web/app/Http/routes.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* API ROUTES FOR MOBILE - JSON WEB TOKEN */
+Route::group(['prefix' => 'api'], function()
+{
+
+    Route::post('login', 'Api\AuthController@login');
+
+    Route::match(['get','post'],'login/{provider?}','Api\AuthController@loginProvider');
+
+    /*
+    Route::get('login/{provider?}','Api\AuthController@loginProvider');
+
+    Route::post('login/{provider?}', 'Api\AuthController@loginProvider');
+    */
+
+    Route::post('register', 'Api\AuthController@register');
+
+    Route::post('logout', 'Api\AuthController@logout');
+
+    Route::group(['middleware' => ['jwt.auth', 'jwt.refresh']], function() {
+
+        Route::get('test', function(){
+            return App\User::all();
+            //return response()->json(['foo'=>'bar']);
+        });
+
+    });
+});
