74b5a69dac0ee25e722d91e8b98969ec31b60ae5
bozidar <bozidar.labas@hotmail.com>
2015-11-21 14:14:57 +0100

    NavigationDrawer added to MainActivity

--- RSC2/app/src/main/java/com/bozidar/labas/microdroid/activities/MainActivity.java
+++ RSC2/app/src/main/java/com/bozidar/labas/microdroid/activities/MainActivity.java
@@ -1,14 +1,35 @@
 package com.bozidar.labas.microdroid.activities;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
 import com.bozidar.labas.microdroid.R;
 import com.bozidar.microdroid.base.MicroActivity;
 
-public class MainActivity extends MicroActivity {
+import butterknife.Bind;
 
+public class MainActivity extends MicroActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @Bind(R.id.toolbar_title)
+    TextView tvToolbar;
+
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+
+    ActionBarDrawerToggle drawerToggle;
 
     @Override
     public int setupToolbar() {
-        return 0;
+        return R.id.toolbar;
     }
 
     @Override
@@ -23,6 +44,55 @@ public class MainActivity extends MicroActivity {
 
     @Override
     public void init() {
-
+        setUpDrawer();
     }
+
+    /**
+     * ------------DRAWER--------------------
+     */
+    private void setUpDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerLayout.setScrimColor(getResources().getColor(android.R.color.transparent));
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("test", "test");
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == com.bozidar.microdroid.R.id.action_search) {
+            Log.d("Search", "Search");
+           // menuSearch();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
