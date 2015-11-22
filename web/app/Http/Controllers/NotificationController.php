d79942af2518ab1853162f63664d4cdb33967595
NikolaSmrcek <zipidus@gmail.com>
2015-11-22 07:06:11 +0100

    Score tracking - eliminiations

--- web/app/Http/Controllers/NotificationController.php
+++ web/app/Http/Controllers/NotificationController.php
@@ -18,6 +18,32 @@ class NotificationController extends Controller
         $sender = Auth::user()->full_name;
         $player = $request->id;
         $message = $request->message;
+        if($message == 'Izbacen si iz igre'){
+            $user = User::where('id',$player)->first();
+            $user->status = 1;
+            $user->save();
+            $game = Game::where('status',1)->first();
+            $teams = $game->teams()->get();
+            for($i = 0;$i < count($teams);$i++){
+                if($teams[$i]->id != $user->team_id){
+                    $team = $teams[$i];
+                    $team->score = $team->score + 2;
+                    $team->save();
+                }
+            }
+
+            if($user->gcm_id){
+                if($user->device == 'android') {
+                    PushNotification::app('android')
+                        ->to($user->gcm_id)
+                        ->send($message);
+                } else {
+                    PushNotification::app('iOS')
+                        ->to($user->gcm_id)
+                        ->send($message);
+                }
+            }
+        }
         $user1 = User::find($player);
 
         if($user1->device == 'android') {
@@ -87,7 +113,27 @@ class NotificationController extends Controller
         $team = Team::where('id',$user->team_id)->first();
         $players = $team->users()->get();
         $message = $request->message;
-
+        if($message == 'Pogoden san'){
+            $user = Auth::user();
+            $user->status = 1;
+            $user->save();
+            $game = Game::where('status',1)->first();
+            $teams = $game->teams()->get();
+            for($i = 0;$i < count($teams);$i++){
+                if($teams[$i]->id != $user->team_id){
+                    $team = $teams[$i];
+                    $team->score = $team->score +2;
+                    /*
+                    if($team->score >= 10){
+                        $game->status = 2;
+                        $team->status = 1;
+                        $game->save();
+                    }
+                    */
+                    $team->save();
+                }
+            }
+        }
         foreach($players as $p){
             if($p->id == $user->id) {
 
