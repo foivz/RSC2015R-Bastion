fa763fd22b7ddc5dca738ada55290839035423c8
Josip Trupina <jtrupina@gmail.com>
2015-11-22 04:25:55 +0100

    judge to players notification

--- web/app/Http/routes.php
+++ web/app/Http/routes.php
@@ -152,9 +152,9 @@ Route::group(['prefix' => 'api'], function()
         Route::post('/teamlist','TeamController@teamsshow');
 
         Route::post('notify/players','NotificationController@judgeToPlayers');
-        Route::post('notify/team','NotificationController@judgeToTeams');
-        //Route::post('notify/players','NotificationController@playersToJudge');
-        //Route::post('notify/players','NotificationController@onlyTeamPlayers');
+        Route::post('notify/teams','NotificationController@judgeToTeams');
+        Route::post('notify/judge','NotificationController@playersToJudge');
+        Route::post('notify/teamplayers','NotificationController@onlyTeamPlayers');
 
     });
 });
